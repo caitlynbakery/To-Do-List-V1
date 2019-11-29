0c0e6d5f299137534e33314b417e2fd42b1e4063
Caitlyn <caitlynbakery@email.com>
2019-11-29 10:16:30 -0800

    video 232 8:23

--- app.js
+++ app.js
@@ -4,9 +4,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.set('view engine', 'ejs');
 
 app.get("/", function(req, res){
-    res.send("Hello");
+
+    var today = new Date();
+    var currentDay = today.getDay();
+
+    if(currentDay === 6 || currentDay === 0) {
+        res.sendFile(__dirname + "/weekend.html");
+    } else {
+        res.sendFile(__dirname + "/weekday.html");
+    }
 });
 
 app.listen(3000, function(){
